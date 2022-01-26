575418c9fd1da513dddd4ea18bd59824aaa3b9af
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2022-01-26 16:05:26 +0400

    completed

---
+++ index.js
@@ -0,0 +1,17 @@
+window.addEventListener('keydown', (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    key.classList.add('playing');
+    audio.play();
+});
+
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return; //it will skip if the propertyname is not transform.
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(element => element.addEventListener('transitionend', removeTransition));
